4c657984489b1342b54cc6a1733f82c4f47d0e3f
Artem Kaloev <kaloev.eltech@gmail.com>
2018-02-19 01:51:52 +0300

    fixed bugs

--- polygon.cpp
+++ polygon.cpp
@@ -6,14 +6,3 @@ polygon::polygon(int x, int y, int r)
     y0 = y;
     R = r;
 }
-//void Model_main::split(int R, int P)
-//{
-//    for (int count = 1; count < 256; count *= 2) {
-//        for (int x = 0; x < R; x += R/count) {
-//            for (int y = 0; y < R; y += R/count)
-//                if (reqSplit(x, y, R/count, P)) {
-//                    draw(x, y, R/count);
-//                }
-//        }
-//    }
-//}
